bc2539ced43db0ed895c08e8498221d0506bffef
Psyakira <91246774+Psyakira@users.noreply.github.com>
2023-03-28 15:05:32 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/test.jpg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="HAIIII" />
+      <HelloWorld msg="HELLOO" />
     </div>
   </header>
 
